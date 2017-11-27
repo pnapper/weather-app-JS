0ba6fadb6a5bf1024bb7ffd10c508c95f7712fec
Daniel Cheng and Gyles Batara <gyles@>
2017-11-27 15:17:22 -0800

    change to javascript api function

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -5,20 +5,46 @@ $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val("");
-    $.ajax({
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}
-`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-      },
-      error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.")
+
+    let request = new XMLHttpRequest();
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        let response = JSON.parse(this.responseText);
+        getElements(response);
       }
-    });
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    const getElements = function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
   });
 });
+
+//jQuery option
+
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     $('#location').val("");
+//     $.ajax({
+//       url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`,
+//       type: 'GET',
+//       data: {
+//         format: 'json'
+//       },
+//       success: function(response) {
+//         $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+//         $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+//       },
+//       error: function() {
+//         $('#errors').text("There was an error processing your request. Please try again.")
+//       }
+//     });
+//   });
+// });
